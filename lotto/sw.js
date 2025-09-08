7528b5180285e040e13bbab8f23bb92d33e5a74f
mishecksimwanza847-source <mishecksimwanza847@gmail.com>
2025-09-08 16:47:58 +0100

    Add files via upload

---
+++ lotto/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "lottery-app-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json"
+];
+
+// Install event
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
